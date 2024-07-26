0ff46675aa166549f0e7aa6af393e1d7896f21f2
ASUS <ASUS@MURARI.mshome.net>
2024-07-26 14:47:44 +0530

    My First Commit

---
+++ OSai/src/main/java/Hello.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Helloooooo World");
+
+		Frame ob=new Frame("Example");
+
+		ob.setSize(300,300);
+
+		ob.setVisible(true);
+
+		ob.setLayout(null);
+	}
+
+}
---
+++ OSai/src/test/java/test/FirstTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+public class FirstTest {
+@Test
+public void methodTest()
+{
+	System.out.println("Helloooom test.....");
+}
+@Test
+public void method1Test()
+{
+	System.out.println("Helloooom test-----222222.....");
+}
+
+}
